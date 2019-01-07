91f98ad1a014464661c7fe4e9f4eba58034f30ad
lepfsd-strapp <lperez@strappinc.net>
2019-01-07 19:01:23 -0400

    first commit

---
+++ src/components/Product.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="product">
+    <img :src="'/dist/' + product.image" class="image">
+    <p class="title">{{product.title}}</p>
+    <p class="price">
+      <span>$ {{product.price}}</span>
+      <span class="qty" v-if="qtyInCart > 0">x{{qtyInCart}}</span>
+    </p>
+    <div class="controls">
+      <button class="add-btn" @click="addToCart" v-if="qtyInCart == 0">ADD TO CART</button>
+      <div class="clearfix" v-else>
+        <button class="inc" @click="inc">+</button>
+        <button class="dec" @click="dec">-</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['product'],
+
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    addToCart(){
+
+    },
+    inc(){
+
+    },
+    dec(){
+
+    }
+  },
+  computed: {
+    qtyInCart (){
+      return 0
+    }
+  }
+}
+</script>
+
+<style>
+.product {
+    float: left;
+    width: 25%;
+    padding: 0.5em 0.5em;
+    margin-bottom: 1em;
+  }
+  .image {
+    display: block;
+    width: 100%;
+  }
+  .title {
+    font-weight: bold;
+    margin: 0.35em 0;
+  }
+  .price {
+    margin: 0.35em 0;
+    font-size: 0.9em;
+  }
+  .qty {
+    float: right;
+  }
+  .add-btn, .inc, .dec {
+    display: block;
+    border: none;
+    padding: 0.5em;
+    outline: none;
+  }
+  .add-btn {
+    width: 100%;
+    background: #41B883;
+    color: #fff;
+  }
+  .inc {
+    width: 49%;
+    background: #418cb8;
+    margin-right: 1%;
+    color: #fff;
+    float: left;
+  }
+  .dec {
+    width: 49%;
+    background: #418cb8;
+    color: #fff;
+    margin-left: 1%;
+    float: left;
+  }
+</style>
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="cart">
+    <p class="cart-title">Shopping Cart</p>
+    <p class="cart-empty" v-if="total == 0">your shopping cart is empty</p>
+    <div class="items" v-else>
+      <div class="item" v-for="item in items">
+        <img :src="'/dist/' + item.image" class="item-image">
+        <div class="item-details">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-price">
+            <span>x{{item.qty}}</span>
+            <span class="right">$ {{item.qty + item.price}}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="cart-total">
+      <span>total</span>
+      <span class="right">$ {{total}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: []
+    }
+  },
+  computed: {
+    total (){
+      return 0
+    }
+  }
+}
+</script>
+
+<style>
+  .cart {
+    margin-left: 1em;
+  }
+  .cart-title {
+    margin: 0.5em 0 0 0;
+    font-weight: bold;
+    text-transform: uppercase;
+    text-align: center;
+    padding: 0.75em;
+    background: #35495E;
+    color: #fff;
+  }
+
+  .cart-empty {
+    text-align: center;
+    margin: 4em 0 0 0;
+    min-height: 300px;
+  }
+  .cart-total {
+    background: #F1F1F1;
+    margin: 0;
+    padding: 0.75em;
+  }
+
+  .items {
+    min-height: 300px;
+  }
+
+  .item {
+    padding: 0.75em 0.5em;
+    border-top: 1px solid #ddd;
+  }
+
+  .right {
+    float: right;
+  }
+
+  .item-image {
+    float: left;
+    width: 20%;
+  }
+  .item-details {
+    float: left;
+    width: 80%;
+    padding-left: 0.75em;
+  }
+  .item-title {
+    margin: 0;
+    font-weight: bold;
+  }
+  .item-price {
+    margin: 0;
+    font-size: 0.9em;
+  }
+</style>
